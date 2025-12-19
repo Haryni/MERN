fa779a5623b950219f143283650f742707a8b550
Haryni <haryni13@gmail.com>
2025-12-19 09:36:58 +0530

    first update

---
+++ backend/src/models/Note.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+//create a schema
+//model based off that schema
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
